08264bbd922e66189018b85dff7964a0af01404d
EC2 Default User <ec2-user@ip-172-31-34-61.ap-northeast-1.compute.internal>
2021-11-14 21:51:12 +0000

    [add]ナビゲーションバー追加 userコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'books#top'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  root 'books#top'
+  resources :users
+  
 end
