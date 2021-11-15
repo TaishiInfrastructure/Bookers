aa9220e72eb7c98aac8f6fee6cb59fd6a371f29c
EC2 Default User <ec2-user@ip-172-31-34-61.ap-northeast-1.compute.internal>
2021-11-15 13:05:19 +0000

    [fix] before_action追記　　ルーティングアクション修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-    
-    
+  before_action :login_user?, only:[:edit, :update, :destroy]
+  before_action :new_book_path, only:[:index, :show]
+
   def index
     @users = User.all
     @user = current_user
@@ -9,23 +10,33 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-   
+    @books = @user.books.reverse
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def create
-  end
-
   def update
-  end
-
-  def destroy
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), success: 'User info was updated successfully.'
+    else
+      render 'edit'
+    end
   end
 
   private
-  
-  
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
+
+  def login_user?
+    user = User.find(params[:id])
+    if user != current_user
+      redirect_to user_path(current_user)
+    end
+  end
+ 
 end
